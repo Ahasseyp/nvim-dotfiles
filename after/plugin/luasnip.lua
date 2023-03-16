e6861b9e231723e40e2dfe5481799b8ae9c75e5b
Albert Hassey <ahasseyp97@gmail.com>
2023-03-16 09:47:28 -0600

    Python snippets

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -6,8 +6,19 @@ local ri = require("luasnip.extras").rep
 
 ls.add_snippets("python", {
     s("radmin", {
-        t("@admin.register("), i(1, "Model"), t({"):", ""}),
-        t("class "), ri(1), t("Admin("), i(2, "admin.ModelAdmin"), t("):"),
+        t("@admin.register("), i(1, "Model"), t(")"),
+        t({"", "class "}), ri(1), t("Admin("), i(2, "admin.ModelAdmin"), t("):"),
         t({"", "\tlist_display = ["}), i(3, "fields"), t("]")
+    })
+})
+
+ls.add_snippets("python", {
+    s("rtype", {
+        t("class "), i(1, "Model"), t("Type(DjangoObjectType):"),
+        t({"", "\t"}), t("class Meta:"),
+        t({"", "\t\t"}), t("model = "), ri(1),
+        t({"", "\t\t"}), t("fields = ["), i(2, "fields"), t("]"),
+        t({"", "\t\t"}), t("filter_fields = {"), i(3, "filter_fields"), t("}"),
+        t({"", "\t\t"}), t("interfaces = (relay.Node, )"),
     })
 })
