2d64d9d054e25d4d3b0f3f3b7a555919ccabbfb3
Albert Hassey <ahasseyp97@gmail.com>
2023-08-30 14:29:11 -0600

    Remove unused plugins

--- lua/ahasseyp/packer.lua
+++ lua/ahasseyp/packer.lua
@@ -125,14 +125,6 @@ return require("packer").startup(function(use)
     use {
         'kevinhwang91/nvim-bqf'
     }
-    use {
-        'mrcjkb/haskell-tools.nvim',
-        requires = {
-            'nvim-lua/plenary.nvim',
-            'nvim-telescope/telescope.nvim', -- optional
-        },
-        branch = '1.x.x', -- recommended
-    }
     use {'junegunn/fzf', run = function()
         vim.fn['fzf#install']()
     end
