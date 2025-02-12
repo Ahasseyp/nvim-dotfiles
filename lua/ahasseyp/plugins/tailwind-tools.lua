8d1ac52014b6a54f0f84efbebb8b3000ea776828
Albert Hassey <ahasseyp97@gmail.com>
2025-02-12 11:28:05 -0600

    chore(updates): updates

--- lua/ahasseyp/plugins/tailwind-tools.lua
+++ lua/ahasseyp/plugins/tailwind-tools.lua
@@ -15,7 +15,7 @@ return {
 			debounce = 200, -- in milliseconds, only applied in insert mode
 		},
 		conceal = {
-			enabled = false, -- can be toggled by commands
+			enabled = true, -- can be toggled by commands
 			min_length = nil, -- only conceal classes exceeding the provided length
 			symbol = "󱏿", -- only a single character is allowed
 			highlight = { -- extmark highlight options, see :h 'highlight'
