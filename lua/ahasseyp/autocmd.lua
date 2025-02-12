0f8d0c25a5a4a5fb1c43f77e7c6c01526f3e2bde
Albert Hassey <ahasseyp97@gmail.com>
2025-02-12 00:39:40 -0600

    chore(all): Remove unused files

--- lua/ahasseyp/autocmd.lua
+++
@@ -1,4 +0,0 @@
-vim.api.nvim_command("autocmd BufWritePre *.py Black")
-vim.api.nvim_command("autocmd BufWritePre *.html,*.css,*.js,*.ts,*.jsx,*.tsx,*.json Prettier")
-vim.api.nvim_command("autocmd BufWritePre *.html,*.css,*.js,*.ts,*.jsx,*.tsx,*.json,.env set nospell")
-vim.api.nvim_command("autocmd BufEnter *.txt set spell")
